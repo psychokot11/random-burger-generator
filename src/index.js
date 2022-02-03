c02e609c6a61771aa9aa1fe26f685ace9b6a5bae
psychokot11 <z.stawarska@gmail.com>
2022-02-03 10:52:03 +0100

    Fetched API, added event listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+const burgerDisplay = document.querySelector(".name-field");
+const button = document.querySelector("img");
 
+//GET
+function getData() {
+  fetch("https://my-burger-api.herokuapp.com/burgers/")
+    .then((response) => response.json())
+    .then((data) => {
+      const randomNumber = Math.floor(Math.random() * data.length);
+      console.log(randomNumber);
+
+      //console.log(data);
+      const name = data[randomNumber].name;
+      //console.log(name);
+      burgerDisplay.innerHTML = name;
+    });
+}
+
+button.addEventListener("click", getData);
